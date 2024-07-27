582f6926acf9f11e6e4d8fa14b7a3a5a428774ef
William John Grace <25494659+wjgra@users.noreply.github.com>
2024-07-27 19:45:53 +0100

    Encapsulate logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,50 @@
-#include <print>
+#include <iostream>
+#include <stdexcept>
 
-import logging;
+#include <boost/program_options.hpp>
 
-using namespace wjgra;
+#include "util/logging.hpp"
 
-int main() {
-    loggingLevel = LOGGING_LEVEL_WARNING;
-    logError("log message... {}, {}, {}", 1, "two", 3);
-    logWarning("log message... {}, {}, {}", 1, "two", 3);
-    logInfo("log message... {}, {}, {}", 1, "two", 3);
-    logDebug("log message... {}, {}, {}", 1, "two", 3);
-    return 0;
+static bool parseOptions(int argc, char** argv) {
+    // Init options parser
+    namespace po = boost::program_options;
+    po::options_description description{"Usage"};
+    description.add_options()
+        ("help", "produce help message")
+        ("logging", po::value<int>()->default_value(util::LOGGING_LEVEL_INFO), util::Logger::helpText().c_str());
+    
+    bool success = true;
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, description), vm);
+        po::notify(vm);    
+        
+        if (vm.contains("help")) {
+            throw std::invalid_argument("help requested");
+        }
+        
+        // Parse logging level
+        if (vm.contains("logging")) {
+            const auto newLevel = static_cast<util::LoggingLevel>(vm["logging"].as<int>());
+            util::Logger::setLoggingLevel(newLevel);
+        }
+        util::logInfo("logging level set to {}", util::Logger::loggingLevelStr());
+
+        // Parse other arguments...
+    }
+    catch (const std::exception& e) {
+        std::println("Displaying usage information ({})", e.what());
+        // Output usage information
+        std::cout << description << '\n';
+        success = false;
+    }
+    return success;
+}
+
+int main(int argc, char** argv) {
+    if (!parseOptions(argc, argv)) {
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
 }
---
+++ src/util/logging.hpp
@@ -0,0 +1,88 @@
+#ifndef _UTIL_LOGGING_HPP_
+#define _UTIL_LOGGING_HPP_
+
+#include <algorithm>
+#include <array>
+#include <print>
+#include <ranges>
+#include <string_view>
+#include <string>
+
+namespace util {
+    enum LoggingLevel {
+        LOGGING_LEVEL_NONE = 0,
+        LOGGING_LEVEL_ERROR,
+        LOGGING_LEVEL_WARNING,
+        LOGGING_LEVEL_INFO,
+        LOGGING_LEVEL_DEBUG
+    };
+
+    struct Logger {
+        static auto getLoggingLevel() {
+            return loggingLevel;
+        }
+
+        static void setLoggingLevel(const LoggingLevel newLevel) {
+            if (!(newLevel >= 0 && newLevel < labels.size())) {
+                throw std::invalid_argument("logging level out of range");
+            }
+            loggingLevel = newLevel;
+        }
+
+        static auto loggingLevelStr(const LoggingLevel level = loggingLevel) {
+            return labels[level];
+        }
+
+        template <class... Args>
+        static void logMessage(const LoggingLevel level, std::format_string<Args...> message, Args&&... args) {
+            if (loggingLevel >= level) {
+                std::print("[ {:^{}} ]: ", labels[level], len_longestLabel);
+                std::println(message, std::forward<Args>(args)...);
+            }
+        }
+
+        static auto helpText() {
+            std::string out = "set logging level (";
+            for (size_t i = 0 ; i < labels.size() ; ++i) {
+                out += std::to_string(i) + " = " + std::string{labels[i]};
+                if (i != labels.size() - 1) {
+                    out += ", ";
+                }
+            }
+            return (out += ")");
+        }
+
+    private:
+        static inline LoggingLevel loggingLevel{LOGGING_LEVEL_INFO};
+        static constexpr auto labels = std::to_array<std::string_view>({
+            "NONE",
+            "ERROR",
+            "WARNING",
+            "INFO",
+            "DEBUG"
+        });
+        static constexpr auto len_longestLabel = std::ranges::max(labels | std::views::transform([](auto&& str){ return str.size(); }));
+    };
+
+    template <class... Args>
+    void logError(std::format_string<Args...> message, Args&&... args) {
+        Logger::logMessage(LOGGING_LEVEL_ERROR, message, std::forward<Args>(args)...);
+    }
+
+    template <class... Args>
+    void logWarning(std::format_string<Args...> message, Args&&... args) {
+        Logger::logMessage(LOGGING_LEVEL_WARNING, message, std::forward<Args>(args)...);
+    }
+
+    template <class... Args>
+    void logInfo(std::format_string<Args...> message, Args&&... args) {
+        Logger::logMessage(LOGGING_LEVEL_INFO, message, std::forward<Args>(args)...);
+    }
+
+    template <class... Args>
+    void logDebug(std::format_string<Args...> message, Args&&... args) {
+        Logger::logMessage(LOGGING_LEVEL_DEBUG, message, std::forward<Args>(args)...);
+    }
+}
+
+#endif
