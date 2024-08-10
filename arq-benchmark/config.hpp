ccf928c7ec556fdb8dccddcf91cd52c514f07856
William John Grace <25494659+wjgra@users.noreply.github.com>
2024-08-10 23:55:57 +0100

    Update launcher configuration handling

---
+++ arq-benchmark/config.hpp
@@ -0,0 +1,43 @@
+#ifndef _ARQ_BENCHMARK_CONFIG_HPP_
+#define _ARQ_BENCHMARK_CONFIG_HPP_
+
+#include <optional>
+#include <stdexcept>
+#include <string_view>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+namespace arq {
+
+    struct config_common {
+        sockaddr_in serverAddr;
+        sockaddr_in clientAddr;
+    };
+
+    struct config_Server {
+
+    };
+
+    struct config_Client {
+
+    };
+
+    struct config_Launcher {
+        config_common common;
+        std::optional<config_Server> server;
+        std::optional<config_Client> client;
+    };
+
+    in_addr stringToSockaddrIn(std::string_view str) {
+        in_addr out;
+        if (inet_aton(str.data(), &out) == 0) {
+            throw std::invalid_argument("invalid IPv4 address format");
+        }
+        return out;
+    }
+    
+}
+
+#endif
--- arq-benchmark/launcher.cpp
+++ arq-benchmark/launcher.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <stdexcept>
+#include <thread>
+
+#include <string>
 
 #include <boost/program_options.hpp>
 
+#include <netinet/in.h>
+
 #include "util/logging.hpp"
 
-static bool parseOptions(int argc, char** argv) {
+#include "config.hpp"
+
+static void configureServer() {
+
+}
+
+static void configureClient() {
+
+}
+
+static bool parseOptions(arq::config_Launcher& config, int argc, char** argv) {
     // Init options parser
     namespace po = boost::program_options;
     po::options_description description{"Usage"};
     description.add_options()
         ("help", "produce help message")
-        ("logging", po::value<int>()->default_value(util::LOGGING_LEVEL_INFO), util::Logger::helpText().c_str());
+        ("logging", po::value<int>()->default_value(util::LOGGING_LEVEL_INFO), util::Logger::helpText().c_str())
+        ("server-addr", po::value<std::string>()->default_value("127.0.0.1"), "server IPv4 address")
+        ("server-port", po::value<in_port_t>()->default_value(0), "server port")
+        ("client-addr", po::value<std::string>()->default_value("127.0.0.1"), "client IPv4 address")
+        ("client-port", po::value<in_port_t>()->default_value(0), "client port")
+        ("launch-server", "start server thread")
+        ("launch-client", "start client thread");
     
     bool success = true;
     try {
@@ -20,7 +41,7 @@ static bool parseOptions(int argc, char** argv) {
         po::notify(vm);    
         
         if (vm.contains("help")) {
-            throw std::invalid_argument("help requested");
+            throw std::invalid_argument("help requested"); // is this the right exception type?
         }
         
         // Parse logging level
@@ -31,8 +52,24 @@ static bool parseOptions(int argc, char** argv) {
         util::logInfo("logging level set to {}", util::Logger::loggingLevelStr());
 
         // Parse other arguments...
+        if (vm.contains("launch-server")) {
+            config.server = arq::config_Server{};
+        }
+
+        if (vm.contains("launch-client")) {
+            config.client = arq::config_Client{};
+        }
+
+        // Move to functions...
+        assert(vm.contains("server-addr")); // change to exception
+        config.common.serverAddr.sin_addr = arq::stringToSockaddrIn(vm["server-addr"].as<std::string>());
+        config.common.serverAddr.sin_port = vm["server-port"].as<in_port_t>();
+
+        assert(vm.contains("client-addr")); // change to exception
+        config.common.clientAddr.sin_addr = arq::stringToSockaddrIn(vm["client-addr"].as<std::string>());
+        config.common.clientAddr.sin_port = vm["client-port"].as<in_port_t>();
     }
-    catch (const std::exception& e) {
+    catch (const std::exception& e) { // specialise this exception handler
         std::println("Displaying usage information ({})", e.what());
         // Output usage information
         std::cout << description << '\n';
@@ -41,10 +78,60 @@ static bool parseOptions(int argc, char** argv) {
     return success;
 }
 
+void startServer(arq::config_Launcher& config) {
+    util::logDebug("attempting to start server");
+
+    auto sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        throw std::runtime_error("Failed to create socket");
+    }
+
+    if (bind(sock, reinterpret_cast<sockaddr*>(&config.common.clientAddr), sizeof(config.common.clientAddr)) == -1) {
+        throw std::runtime_error("Failed to bind socket");
+    }
+
+    if (listen(sock, 50 /* number of connection requests - listen backlog */) == -1) {
+        throw std::runtime_error("Failed to listen");
+    }
+
+    socklen_t sizeof_serverAddr = sizeof(config.common.serverAddr);
+    auto accepted = accept(sock, reinterpret_cast<sockaddr*>(&config.common.serverAddr), &sizeof_serverAddr);
+    if (accepted == -1) {
+        throw std::runtime_error("Failed to accept");
+    }
+    util::logInfo("server connected");
+
+    if (close(sock) == -1) {
+        throw std::runtime_error("Failed to close");
+    }
+}
+
+void startClient(arq::config_Launcher& config) {
+
+}
+
 int main(int argc, char** argv) {
-    if (!parseOptions(argc, argv)) {
+    arq::config_Launcher cfg; // reformulate to use auto X = make() pattern
+    if (!parseOptions(cfg, argc, argv)) {
         return EXIT_FAILURE;
     }
+
+    std::thread serverThread, clientThread;
+    if (cfg.server.has_value()) {
+        serverThread = std::thread(startServer, std::ref(cfg));
+    }
+
+    if (cfg.client.has_value()) {
+        clientThread = std::thread(startClient, std::ref(cfg));
+    }
+
+    if (serverThread.joinable()) {
+        serverThread.join();
+    }
     
+    if (clientThread.joinable()) {
+        clientThread.join();
+    }
+
     return EXIT_SUCCESS;
 }
