46f6cffd221f5a80156ea8574035c776b2c7474e
William John Grace <25494659+wjgra@users.noreply.github.com>
2024-10-19 11:07:45 +0100

    Reorganise namespaces; add configurable delay

--- test_scripts/run.sh
+++ test_scripts/run.sh
@@ -5,40 +5,49 @@ WRAP=${WRAP} # Allows scripts to be run under GDB, for instance
 BASE_DIR=".."
 
 VETH_NAME="arqveth"
-NS_NAME="arqns0"
+NS_NAME="arqns"
 
-# Create veth link
-ip link delete ${VETH_NAME}0 || true
-ip link delete ${VETH_NAME}1 || true
-ip link add dev ${VETH_NAME}0 type veth peer ${VETH_NAME}1 mtu 1500
+server_veth="arqveth0"
+client_veth="arqveth1"
 
-ip addr add 10.0.0.1 dev ${VETH_NAME}0
-# ip addr add 10.0.0.2 dev ${VETH_NAME}1
+server_ns="arqns0"
+client_ns="arqns1"
 
-# Create network namespaces
-ip netns add ${NS_NAME}
-ip netns exec ${NS_NAME} ip link set dev ${VETH_NAME}1 up
-ip netns exec ${NS_NAME} ip addr add 10.0.0.2 dev ${VETH_NAME}1
-ip netns exec ${NS_NAME} ip route add default via 10.0.0.1 dev ${VETH_NAME}1
+server_addr="10.0.0.1"
+client_addr="10.0.0.2"
 
-iptables -t nat -A POSTROUTING -o ${VETH_NAME}0 - j MASQUERADE
-iptables -A FORWARD  -i ${VETH_NAME}0 -J ACCEPT
-iptables -A FORWARD  -o ${VETH_NAME}0 -J ACCEPT
+tx_delay="1000ms"
 
+# Clean up old namespaces
+ip netns delete ${server_ns} || true
+ip netns delete ${client_ns} || true
+
+# Create namespaces and veth pair
+ip netns add ${server_ns}
+ip netns add ${client_ns}
+ip link add name ${server_veth} netns ${server_ns} type veth peer name ${client_veth} netns ${client_ns} mtu 1500
+
+ip netns exec ${server_ns} ip addr add ${server_addr} dev ${server_veth}
+ip netns exec ${client_ns} ip addr add ${client_addr} dev ${client_veth}
+
+# ip netns exec ${server_ns} ip link set lo up
+# ip netns exec ${client_ns} ip link set lo up
+ip netns exec ${server_ns} ip link set ${server_veth} up
+ip netns exec ${client_ns} ip link set ${client_veth} up
+
+ip netns exec ${server_ns} ip route add ${client_addr} dev ${server_veth}
+ip netns exec ${client_ns} ip route add ${server_addr} dev ${client_veth}
 
 # Simulate network conditions
-# tc...
-# tc qdisc add dev arqveth0 root netem delay 1000ms
+ip netns exec ${server_ns} tc qdisc add dev ${server_veth} root netem delay ${tx_delay}
 
-# Bring link up
-ip link set dev ${VETH_NAME}0 up
-# ip link set dev ${VETH_NAME}1 up
+# Start server
+tmux new-session -d -s "arq" -n "server" -c "${BASE_DIR}" "ip netns exec ${server_ns} ${WRAP} build/arq-benchmark/launcher --launch-server --logging 4 --client-addr ${client_addr} --server-addr ${server_addr}; read"
 
-tmux new-session -d -s "arq" -n "server" -c "${BASE_DIR}" "ip netns exec ${NS_NAME} ${WRAP} build/arq-benchmark/launcher --launch-server --logging 4 --client-addr 10.0.0.1 --server-addr 10.0.0.2; read"
-
-tmux new-window -t "arq" -n "client" -c "${BASE_DIR}" "${WRAP} build/arq-benchmark/launcher --launch-client --logging 4 --client-addr 10.0.0.1 --server-addr 10.0.0.2; read"
+# Start client
+tmux new-window -t "arq" -n "client" -c "${BASE_DIR}" "ip netns exec ${client_ns} ${WRAP} build/arq-benchmark/launcher --launch-client --logging 4 --client-addr ${client_addr} --server-addr ${server_addr}; read"
 
 tmux set-option -t "arq" -g mouse
 tmux attach-session -t "arq"
 
-tc qdisc del dev arqveth0 root netem delay 100ms
+ip netns exec ${server_ns} tc qdisc del dev ${server_veth} root netem delay ${tx_delay}
