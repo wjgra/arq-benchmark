eb590f6aa572522f4e1577a139fe9006d232d2cc
William John Grace <25494659+wjgra@users.noreply.github.com>
2024-11-25 17:03:47 +0000

    Control packet serialisation

---
+++ arq-benchmark/arq/control_packet.cpp
@@ -0,0 +1,9 @@
+#include "arq/control_packet.hpp"
+
+bool arq::ControlPacket::serialise(std::span<std::byte> buffer) const noexcept {
+    return serialiseSeqNum(sequenceNumber_, buffer);
+}
+
+bool arq::ControlPacket::deserialise(std::span<const std::byte> buffer) noexcept {
+    return deserialiseSeqNum(sequenceNumber_, buffer);
+}
